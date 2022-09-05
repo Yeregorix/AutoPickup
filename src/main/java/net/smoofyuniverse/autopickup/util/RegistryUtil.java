7a092219562b7cafd1bca3d74782f9da7c21c938
Yeregorix <yeregorixcraft@gmail.com>
2022-09-05 20:31:37 +0200

    Fix pattern matching of block states

--- src/main/java/net/smoofyuniverse/autopickup/util/RegistryUtil.java
+++ src/main/java/net/smoofyuniverse/autopickup/util/RegistryUtil.java
@@ -67,7 +67,7 @@ public class RegistryUtil {
 
 				Stream<BlockState> matchedStates = blockTypeRegistry.stream()
 						.flatMap(type -> type.validStates().stream())
-						.filter(state -> pattern.matcher(state.toString()).matches());
+						.filter(state -> pattern.matcher(state.asString()).matches());
 
 				if (negate)
 					matchedStates.forEach(states::remove);
